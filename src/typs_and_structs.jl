8aa844d0ee110980a6270fb4e781c0eb476d29e5
Zitzeronion <physiknerd@gmail.com>
2020-04-28 09:14:33 +0200

    Multiple dispatch for Float32 and Float64 equilibria

--- src/typs_and_structs.jl
+++ src/typs_and_structs.jl
@@ -1,23 +1,13 @@
-using StaticArrays
 using DelimitedFiles
 
 # lattice Boltzmann equation related stuff in one and two dimension
 abstract type moments end
 abstract type distribution end
 
-input = readdlm("../input/input.txt")
-len = input[1,2]
-
 mutable struct moments1d_64 <: moments
     height::Vector{Float64}
     velocity::Vector{Float64}
     energy::Vector{Float64}
-end
-
-mutable struct moments1d_static <: moments
-    height::MVector{len, Float64}
-    velocity::MVector{len, Float64}
-    energy::MVector{len, Float64}
 end
 
 mutable struct moments1d_32 <: moments
@@ -26,15 +16,28 @@ mutable struct moments1d_32 <: moments
     energy::Float32
 end
 
-mutable struct distribution_1d <: distribution
+mutable struct distribution64_1d <: distribution
     f0::Vector{Float64}
     f1::Vector{Float64}
     f2::Vector{Float64}
 end
 
-mutable struct distributions1d_static <: distribution
+mutable struct distribution32_1d <: distribution
+    f0::Vector{Float32}
+    f1::Vector{Float32}
+    f2::Vector{Float32}
+end
 
-    
+mutable struct distribution64_2d <: distribution
+    f0::Vector{Float64}
+    f1::Vector{Float64}
+    f2::Vector{Float64}
+    f3::Vector{Float64}
+    f4::Vector{Float64}
+    f5::Vector{Float64}
+    f6::Vector{Float64}
+    f7::Vector{Float64}
+    f8::Vector{Float64}
 end
 
 
